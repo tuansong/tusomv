db1c442557696a9eb1ab6050ea01de1397ba0c0c
tuansong <trantuansong22@gmail.com>
2018-09-30 21:58:05 +0700

    update ui

--- src/components/MovieList/MovieDetail/MovieDetail.js
+++ src/components/MovieList/MovieDetail/MovieDetail.js
@@ -13,7 +13,7 @@ class MovieDetail extends Component {
     }
 
     getDateFormat = (date) => {
-        let newDate = date.slice(8) + date.slice(4,7)+ date.slice(7, 8) + date.slice(0,4);
+        let newDate = date.slice(8) + date.slice(4, 7) + date.slice(7, 8) + date.slice(0, 4);
         return newDate;
     }
 
@@ -28,22 +28,24 @@ class MovieDetail extends Component {
                         <div className="movie-rating">
                             <button>{this.props.rating}<i className="fas fa-star" style={{ color: 'yellow' }}></i></button>
                             <a onClick={() => this.setState({ liked: !this.state.liked })}>
-                                {this.state.liked ? <i className="fas fa-heart" style={{color:'red', fontSize:'1.5em'}}></i> 
-                                : <i className="far fa-heart" style={{fontSize:'1.5em'}}></i>}
+                                {this.state.liked ? <i className="fas fa-heart" style={{ color: 'red', fontSize: '1.5em' }}></i>
+                                    : <i className="far fa-heart" style={{ fontSize: '1.5em' }}></i>}
                             </a>
                         </div>
                         <div className="movie-title">
                             <h3>{this.props.title}</h3>
                         </div>
                     </div>
-                    <CardBody style={{height: '200px', overflowY: 'auto', paddingBottom: '0'}}>
+                    <CardBody style={{ height: '200px', overflowY: 'auto', paddingBottom: '0' }}>
                         <CardText>
-                            <span className="desc">Story:</span> 
-                            {this.props.overview.length > 200 ? this.props.overview.slice(0, 200) + '...' : this.props.overview} 
+                            <span className="desc">Story:</span>
+                            {this.props.overview.length > 200 ?
+                                this.props.overview.slice(0, 200) + '...' :
+                                this.props.overview}
                             <a className="view-more"
                                 onClick={this.props.openModal}
-                                >View more
-                            </a>    
+                            >View more
+                            </a>
                         </CardText>
                         <div className="date">{this.getDateFormat(this.props.date)}</div>
                     </CardBody>
